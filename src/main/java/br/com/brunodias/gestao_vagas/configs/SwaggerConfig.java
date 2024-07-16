7ed7400424c2e27d124609ec2b349cf2f69dd09d
brunodias77 <brunohdias95@gmail.com>
2024-07-16 01:32:16 -0300

    feat: Add SwaggerConfig, ListAllJobsByFilterUseCase, AuthCompanResponseDTO, and ListAllJobsByCompanyUseCase classes

---
+++ src/main/java/br/com/brunodias/gestao_vagas/configs/SwaggerConfig.java
@@ -0,0 +1,2 @@
+package br.com.brunodias.gestao_vagas.configs;public class SwaggerConfig {
+}
---
+++ src/main/java/br/com/brunodias/gestao_vagas/modules/company/dtos/AuthCompanResponseDTO.java
@@ -0,0 +1,2 @@
+package br.com.brunodias.gestao_vagas.modules.company.dtos;public class AuthCompanResponseDTO {
+}
